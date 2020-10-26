2fe7a754e5fdb094b5dfc05f2480ce48641a0aad
Anand Sambamoorthy <anand.sambamoorthy@gmail.com>
2020-10-25 19:00:55 -0500

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=bpsirmione
-export LIMONE_ALIAS=bplimone
-export SCORPIO_ALIAS=bpscorpio
-export VIRGO_ALIAS=bpvirgo
-export LIBRA_ALIAS=bplibra
-export ZODIAC_ALIAS=bpzodiac
-export DB_ADMIN_USER=nick
+export SIRMIONE_ALIAS=AS7531zodiac
+export LIMONE_ALIAS=AS7531zodiac
+export SCORPIO_ALIAS=AS7531zodiac
+export VIRGO_ALIAS=AS7531zodiac
+export LIBRA_ALIAS=AS7531zodiac
+export ZODIAC_ALIAS=AS7531zodiac
+export DB_ADMIN_USER=samba0
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
